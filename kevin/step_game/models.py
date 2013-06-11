d168fd403b422f0e2fa0b047f5d6a7c17453459e
Paul Whipp <paul.whipp@gmail.com>
2013-06-11 10:35:24 +1000

    Added unicode to models for easier working

--- kevin/step_game/models.py
+++ kevin/step_game/models.py
@@ -1,11 +1,8 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#     * Rearrange models' order
-#     * Make sure each model has one field with primary_key=True
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
+"""This is the supplied model for the demo converted directly into an
+ORM (with no relations to speak of ;). It can be substantially
+improved by correctly defining the relationships. Until that is done,
+the admin support will be patchy because there are tables with no key
+fields etc."""
 from __future__ import unicode_literals
 
 from django.db import models
@@ -17,6 +14,9 @@ class Actors(models.Model):
     gender = models.CharField(max_length=1L, blank=True)
     class Meta:
         db_table = 'actors'
+    def __unicode__(self):
+        return "{0}, {1}".format(self.last_name, self.first_name)
+    
 
 class Directors(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -24,13 +24,17 @@ class Directors(models.Model):
     last_name = models.CharField(max_length=100L, blank=True)
     class Meta:
         db_table = 'directors'
+    def __unicode__(self):
+        return "{0}, {1}".format(self.last_name, self.first_name)
 
 class DirectorsGenres(models.Model):
-    director_id = models.IntegerField(null=True, blank=True)
+    director_id = models.IntegerField(primary_key=True, blank=True)
     genre = models.CharField(max_length=100L, blank=True)
     prob = models.FloatField(null=True, blank=True)
     class Meta:
         db_table = 'directors_genres'
+    def __unicode__(self):
+        return "({0}) {1}".format(self.director_id, self.genre)
 
 class Movies(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -39,18 +43,24 @@ class Movies(models.Model):
     rank = models.FloatField(null=True, blank=True)
     class Meta:
         db_table = 'movies'
+    def __unicode__(self):
+        return "{0} ({1})".format(self.name, self.year)
 
 class MoviesDirectors(models.Model):
     director_id = models.IntegerField(null=True, blank=True)
     movie_id = models.IntegerField(null=True, blank=True)
     class Meta:
         db_table = 'movies_directors'
+    def __unicode__(self):
+        return "{0}-{1}".format(self.director_id, self.movie_id)
 
 class MoviesGenres(models.Model):
     movie_id = models.IntegerField(null=True, blank=True)
     genre = models.CharField(max_length=100L, blank=True)
     class Meta:
         db_table = 'movies_genres'
+    def __unicode__(self):
+        return "({0}) {1}".format(self.movie_id, self.genre)
 
 class Roles(models.Model):
     actor_id = models.IntegerField(null=True, blank=True)
@@ -58,4 +68,6 @@ class Roles(models.Model):
     role = models.CharField(max_length=100L, blank=True)
     class Meta:
         db_table = 'roles'
+    def __unicode__(self):
+        return "{0}-{1}-{2}".format(self.actor_id, self.movie_id, self.role)
 
