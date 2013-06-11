2e1de0a05368333b41d33637f760690092415386
Paul Whipp <paul.whipp@gmail.com>
2013-06-11 18:59:34 +1000

    Added in form and form redirection

--- kevin/step_game/views.py
+++ kevin/step_game/views.py
@@ -1,20 +1,43 @@
-from step_game.forms import ActorForm
-from django.views.generic.edit import FormView
+from step_game.forms import ActorSearchForm
+from django.views.generic import TemplateView
 from django.views.generic import ListView
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 from step_game.models import Movie, Actor
 
-class MainView(FormView):
+class SearchFormMixin(object):
+    """Mixin behaviour for the search form"""
+    form_class = ActorSearchForm
+    def get_context_data(self, **kwargs):
+        """Specialisation to add actor_search_form to context"""
+        context = super(SearchFormMixin, self).get_context_data(**kwargs)
+        context['actor_search_form'] = ActorSearchForm(self.request.POST)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """Specialisation to redirect to result using valid form data"""
+        form = ActorSearchForm(request.POST)
+        last_name = request.POST['last_name']
+        first_name = request.POST['first_name']
+        if form.is_valid():
+            try:
+                actor = Actor.objects.get(last_name=last_name, first_name=first_name)
+                return redirect('result', last_name=last_name, first_name=first_name)
+            except Actor.DoesNotExist:
+                pass
+
+        msg = "Sorry: '{0}', '{1}' is not a known actor.".format(last_name, first_name)
+        messages.add_message(request, messages.WARNING, msg)
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+            
+    
+class MainView(SearchFormMixin, TemplateView):
     template_name = 'step_game/main.html'
-    form_class = ActorForm
 
-    def form_valid(self, form):
-        """create /result/<last_name>/<first_name>/"""
-        self.success_url = "/result/Affleck/Casey/"
-        return super(MainView, self).form_valid(form)
 
-class ResultView(ListView):
+class ResultView(SearchFormMixin, ListView):
     template_name = 'step_game/result.html'
     model = Movie
     context_object_name = "movie_list"
@@ -41,7 +64,6 @@ class ResultView(ListView):
 
         context['actor'] = self.actor
         context['shared_movie_list'] = self.shared_movies
-        
         return context
     
 
